717e0dd7afbc9ebbe0c356431279e20e7efe0519
Oneul1213 <toda1213@naver.com>
2024-11-09 22:47:14 +0900

    [FIX]#29/Divider mx 미적용 오류 임시 수정

--- components/Common/Divider.tsx
+++ components/Common/Divider.tsx
@@ -1,16 +1,12 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
+import { twMerge } from "tailwind-merge"
 
-export default function Divider({ gap, parentPadding }: { gap?: string, parentPadding?: string }) {
-    const [windowWidth, setWindowWidth] = useState<number>()
-
-    useEffect(() => {
-        setWindowWidth(window.innerWidth)
-    }, [])
-    
+export default function Divider({ gap }: { gap?: string }) {
+    // TODO :  mx-[-1.5rem] 하드코딩 수정할 방법 찾기...
     return (
         <div
             id="root"
-            className={`w-[100vw] h-[0.1rem] ${parentPadding ? "mx-[-" + parentPadding + "]" : ""} bg-rsgGreen-primary`}
+            className={`w-[100vw] h-[0.1rem] bg-rsgGreen-primary mx-[-1.5rem]`}
             style={{ marginTop: gap, marginBottom: gap }}>
         </div>
     )
