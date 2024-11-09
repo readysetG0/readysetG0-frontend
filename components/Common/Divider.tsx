121bb249d14cb172829aa8637ebcb914865778fd
Oneul1213 <toda1213@naver.com>
2024-11-09 22:23:21 +0900

    [FIX]#29/Divider 고정 오류 수정

--- components/Common/Divider.tsx
+++ components/Common/Divider.tsx
@@ -1,5 +1,17 @@
-export default function Divider({ gap }: { gap?: string }) {
+import { useEffect, useState } from "react"
+
+export default function Divider({ gap, parentPadding }: { gap?: string, parentPadding?: string }) {
+    const [windowWidth, setWindowWidth] = useState<number>()
+
+    useEffect(() => {
+        setWindowWidth(window.innerWidth)
+    }, [])
+    
     return (
-        <div id="root" className="fixed left-0 right-0 w-full h-[0.1rem] bg-rsgGreen-primary" style={{ marginTop: gap, marginBottom: gap }}></div>
+        <div
+            id="root"
+            className={`w-[100vw] h-[0.1rem] ${parentPadding ? "mx-[-" + parentPadding + "]" : ""} bg-rsgGreen-primary`}
+            style={{ marginTop: gap, marginBottom: gap }}>
+        </div>
     )
 }
